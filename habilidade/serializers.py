742d4ddf114fe2e98c29f8b4ddce3fe46732d88f
samantasouza357 <samantasouza931@gmail.com>
2019-09-27 17:00:46 -0300

    adicionar modelsde habilidade

---
+++ habilidade/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+from habilidade.models import  Habilidade
+
+
+class HabilidadeSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    nome = serializers.CharField(max_length=255)
+
+    def create(self, validated_data):
+        habilidade = Habilidade.objects.create(**validated_data)
+        return habilidade
+
+
+    def update(self, instance, validated_data):
+         instance.nome = validated_data.get('nome')
+         instance.save()
+         return instance
