12c87b681acd4493a3fda2abfa21f7bc680a0c65
Geovanni Martins <geeo.martiins14@gmail.com>
2019-09-27 15:55:34 -0300

    Adicionar models Categoria

--- cat_heroi/models.py
+++ cat_heroi/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Categoria(models.Model):
+    nome = models.Chafield(
+        max_lenght=255,
+        verbose_name='Nome Categoria'
+    )
--- cat_heroi/views.py
+++ cat_heroi/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.filters import SearchFilter
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from cat_heroi.models import Categoria
+from cat_heroi.serializers import CategoriaSerializer
 
-# Create your views here.
+
+class CategoriaViewSet(viewsets.ModelViewSet):
+    filter_backends = [SearchFilter]
+    search_fields = ['Nome Categoria']
+    queryset = Categoria.objects.all()
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+    authentication_classes = (TokenAuthentication,)
+    serializer_class = CategoriaSerializer
