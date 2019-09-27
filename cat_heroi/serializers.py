94874bb26cadf5bd47390ea50c34b2be38c9dabd
Geovanni Martins <geeo.martiins14@gmail.com>
2019-09-27 16:41:26 -0300

    Adicionar models cat_heroi

--- cat_heroi/serializers.py
+++ cat_heroi/serializers.py
@@ -3,6 +3,18 @@ from .models import Categoria
 
 
 class CategoriaSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Categoria
-        field = ('Nome Categoria')
+    id = serializers.IntegerField(read_only=True)
+    nome = serializers.CharField(max_length=255)
+
+    def create(self, validated_data):
+        categoria = Categoria.objects.create(**validated_data)
+        return categoria
+
+    def update(self, instance, validated_data):
+        instance.nome = validated_data.get('nome')
+        instance.save()
+        return instance
+
+class CategoriaLightSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    nome = serializers.CharField()
--- cat_heroi/views.py
+++ cat_heroi/views.py
@@ -1,10 +1,12 @@
+from django.http import Http404
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, views, status
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.filters import SearchFilter
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from cat_heroi.models import Categoria
-from cat_heroi.serializers import CategoriaSerializer
+from cat_heroi.serializers import CategoriaSerializer, CategoriaLightSerializer
+from rest_framework.response import Response
 
 
 class CategoriaViewSet(viewsets.ModelViewSet):
@@ -14,3 +16,38 @@ class CategoriaViewSet(viewsets.ModelViewSet):
     permission_classes = (IsAuthenticatedOrReadOnly,)
     authentication_classes = (TokenAuthentication,)
     serializer_class = CategoriaSerializer
+
+
+class CategoriaList(views.APIView):
+    def get(self, request):
+        categorias = Categoria.objects.all()
+        serializer = CategoriaLightSerializer(categorias, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        serializer = CategoriaSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class CategoriaDetails(views.APIView):
+    def get_object(self, id):
+        try:
+            return Categoria.objects.get(id=id)
+        except:
+            raise Http404
+
+    def get(self, request, id):
+        categoria = self.get_object(id)
+        serializer = CategoriaSerializer(categoria)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def put(self, request, id):
+        categoria = self.get_object(id)
+        serializer = CategoriaSerializer(categoria, data=request.data)
+        if serializer .is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.error, status=status.HTTP_400_BAD_REQUEST)
