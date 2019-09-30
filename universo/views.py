86a4866b100e2bddf0a7137a0a00fb8e8bcdbe32
samantasouza357 <samantasouza931@gmail.com>
2019-09-30 17:03:47 -0300

    alteracao

--- universo/views.py
+++ universo/views.py
@@ -1,17 +1,13 @@
-from django.http import Http404
-from django.shortcuts import render
-from rest_framework import viewsets, status, views
-from rest_framework.authentication import TokenAuthentication
+
+from rest_framework import viewsets
 from rest_framework.filters import SearchFilter
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
-from rest_framework.response import Response
 from universo.models import Universo
-from universo.serializers import UniversoSerializer, UniversoLightSerializer
+from universo.serializers import UniversoSerializer
 
 
 class UniversoViewSet(viewsets.ModelViewSet):
     filter_backends = [SearchFilter]
-    search_fields = ['Nome Universo']
+    # search_fields = ['Nome Universo']
     queryset = Universo.objects.all()
     # permission_classes = (IsAuthenticatedOrReadOnly,)
     # authentication_classes = (TokenAuthentication,)
