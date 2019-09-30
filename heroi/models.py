901e3d80848a2965672de25fee67fe3119928822
samantasouza357 <samantasouza931@gmail.com>
2019-09-30 14:44:03 -0300

    adicionar relacionamento de herois

--- heroi/models.py
+++ heroi/models.py
@@ -2,6 +2,9 @@ from django.db import models
 
 
 # Create your models here.
+from cat_heroi.models import Categoria
+from habilidade.models import Habilidade
+from universo.models import Universo
 
 
 class Heroi(models.Model):
@@ -11,4 +14,19 @@ class Heroi(models.Model):
     )
     idade = models.IntegerField(
         verbose_name='idade'
+    )
+    heroi_uni = models.ForeignKey(
+        Universo,
+        on_delete=models.CASCADE,
+        related_name='heroi_uni'
+    )
+    heroi_hab = models.ManyToManyField(
+        Habilidade,
+        # on_delete=models.CASCADE,
+        related_name='heroi_hab'
+    )
+    heroi_cat = models.ForeignKey(
+        Categoria,
+        on_delete=models.CASCADE,
+        related_name='heroi_cat'
     )
