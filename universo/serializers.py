cf4f9f9760c440a13de90190c84219c4c31a04a5
samantasouza357 <samantasouza931@gmail.com>
2019-10-01 14:29:24 -0300

    alteracao

--- universo/serializers.py
+++ universo/serializers.py
@@ -2,11 +2,13 @@ from rest_framework import serializers
 from .models import Universo
 
 
-class UniversoSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Universo
-        fields = ('id', 'nome')
+class UniversoSerializer(serializers.Serializer):
+   id =serializers.IntegerField(read_only=True)
+   nome = serializers.CharField(max_length=255)
 
+   def create(self, validated_data):
+       universo = Universo.objects.create(**validated_data)
+       return universo
 
 class UniversoLightSerializer(serializers.Serializer):
     id = serializers.IntegerField()
